05dd824a06f8f648e3823fd5904175932bff0525
Martin Kostolný <clearmartin@zoho.com>
2017-12-28 10:35:44 +0100

    removed debug logging & bumped version

--- app-side/launcher_api_firefox_stdin.py
+++ app-side/launcher_api_firefox_stdin.py
@@ -4,18 +4,6 @@ import sys, json, struct, subprocess
 
 application = 'firefox'
 
-# debug logging - TODO remove!!!
-import logging
-from logging.handlers import RotatingFileHandler
-#logging.basicConfig(filename='launcher_api_firefox_stdin.log', level=logging.DEBUG, format='%(asctime)s [%(levelname)s] (%(threadName)s) %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger('file_logger')
-logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler("launcher_api_firefox_stdin.log", maxBytes=1024*1024, backupCount=1)
-formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)s] {%(process)d} (%(threadName)s) %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-# debug logging - TODO remove!!!
-
 # try libunity
 launcher = None
 loop = None
@@ -34,22 +22,22 @@ try:
     try:
         import _thread
         _thread.start_new_thread(loop.run, ())
-        logger.debug('thread started with loop.run()')
+        print('thread started with loop.run()')
     except:
-        logger.debug('Error creating a thread')
+        print('Error creating a thread')
 except:
-    logger.debug('libunity not found')
+    print('libunity not found')
 
 # read a message from stdin and decode it.
 def readMessage():
     rawLength = sys.stdin.buffer.read(4)
     if len(rawLength) == 0:
-        logger.debug('length is empty, exiting')
+        print('length is empty, exiting')
         sys.exit(0)
     messageLength = struct.unpack('@I', rawLength)[0]
     message = sys.stdin.read(messageLength)
     receivedMessage = json.loads(message)
-    logger.debug('receivedMessage: ' + receivedMessage)
+    print('receivedMessage: ' + receivedMessage)
     return receivedMessage
 
 
@@ -68,9 +56,9 @@ def processMessage(receivedMessage):
     try:
         count = int(countMessage[6:])
         if (count < 0 or count > 9999):
-            logger.debug("Count has to be in range 0...9999.")
+            print("Count has to be in range 0...9999.")
         elif (count == previousCount):
-            logger.debug("Count has not changed.")
+            print("Count has not changed.")
         else:
             previousCount = count
             if (count == 0):
@@ -96,7 +84,7 @@ def processMessage(receivedMessage):
                 else:
                     subprocess.run(["gdbus", "emit", "--session", "--object-path", "/", "--signal", "com.canonical.Unity.LauncherEntry.Update", application, "{'progress-visible': <'true'>, 'count-visible': <'true'>, 'count': <'%d'>}" % count])
     except:
-        logger.debug("Error parsing count value.")
+        print("Error parsing count value.")
 
     #
     # handle message with PROGRESS
@@ -104,12 +92,12 @@ def processMessage(receivedMessage):
     try:
         progress = round(float(progressMessage[9:]), 2)
         if (progress < 0 or progress > 1):
-            logger.debug("Progress has to be in range 0...1.")
+            print("Progress has to be in range 0...1.")
         elif (progress == previousProgress):
-            logger.debug("Progress has not changed.")
+            print("Progress has not changed.")
         else:
             previousProgress = progress
-            logger.debug('setting progress=' + str(progress))
+            print('setting progress=' + str(progress))
             #
             # set task manager entry's 'progress'
             #
@@ -118,10 +106,10 @@ def processMessage(receivedMessage):
             else:
                 subprocess.run(["gdbus", "emit", "--session", "--object-path", "/", "--signal", "com.canonical.Unity.LauncherEntry.Update", application, "{'progress-visible': <'true'>, 'progress': <'%.4f'>}" % progress])
     except:
-        logger.debug("Error parsing progress value.")
+        print("Error parsing progress value.")
 
 
-logger.debug('start listening for messages')
+print('start listening for messages')
 while True:
     receivedMessage = readMessage()
     processMessage(receivedMessage)
